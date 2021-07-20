e91e47068730c696ca7623d1f8284967d607fbc5
Oisín_windows <byrne.ois@gmail.com>
2021-07-20 12:12:12 +0100

    netlify function test

--- pages/guides.js
+++ pages/guides.js
@@ -1,6 +1,27 @@
+import { useEffect } from 'react'
 import styles from '../styles/Guides.module.css'
 
 export default function Guides() {
+
+  useEffect(()=>{
+    // fetch('/.netlify/functions/supermario')
+    //   .then(res => res.json())
+    //   .then(data => console.log(data))
+
+    const getData = async () => {   
+      // relatiuve path to Netlify function endpoint
+      const res = await fetch('/.netlify/functions/supermario')
+      const data = await res.json()
+      console.log(data);
+    } 
+
+    getData()
+
+
+  },[])
+
+
+
   return (
     <div className={styles.guides}>
       <h2>All Guides</h2>
